85106f020d0ed360dcc36462c5ba31bef8fd16f6
Innovate81 <sonnymyates@gmail.com>
2025-06-24 19:40:06 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,6 @@
-import '../styles/globals.css'; // keep this if you’re using it
-import Navbar from '../components/navbar'; // adjust if your filename is Navbar.js
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
-}
-
-export default MyApp;
+return (
+  <>
+    <Navbar />
+    <Component {...pageProps} />
+  </>
+);
