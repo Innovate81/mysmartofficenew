c0e2a90ab0aead302ee0a4ab79b727efe6b48f73
Innovate81 <sonnymyates@gmail.com>
2025-06-24 19:47:45 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import '../styles/globals.css'; // keep if you're using Tailwind or global styles
+import Navbar from '../components/Navbar';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
